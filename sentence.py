0b3935a2ff0824c775fd474336bb726ae8e4d28d
wward <woward00@gmail.com>
2021-10-09 15:54:33 -0600

    sentence for literals resolved

--- sentence.py
+++ sentence.py
@@ -8,19 +8,20 @@ class Sentence:
                  negation: bool = False) -> None:
         self.name: str = name
         self.identifier: str = identifier
-        self.literals = List[int] if literals is None else literals
+        self.literals: List[int] = [] if literals is None else literals
         self.variables: List[str] = [] if variables is None else variables
-        self.vars: bool = True if self.variables is not None else False
+        self.vars: bool = True if len(self.variables) != 0 else False
+        self.verbose: bool = False
         self.negation: bool = negation
         self.string: str = ""
 
     def __str__(self) -> str:  # TODO anytime a sentence is modified self.string MUST be set to ""
-        if self.string == "":
-            string: str = f"{'~' if self.negation else ''}{self.identifier}("
+        if self.string == "" or self.verbose is True:
+            string: str = f"{'~' if self.negation else ''}{self.name if self.verbose else self.identifier}("
             arguments = self.variables if self.vars else self.literals
-            for arg in arguments:
-                string += str(arg)
-                if arguments.index(arg) != len(arguments)-1:
+            for i in range(len(arguments)):
+                string += str(arguments[i])
+                if i != len(arguments)-1:
                     string += ", "
             string += ")"
             self.string = deepcopy(string)
