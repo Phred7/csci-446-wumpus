ebd9c596920d28695e53ef9ba6944884ce02a2da
linknmike <mikeheidal@gmail.com>
2021-10-13 15:30:28 -0600

    Fixed knowledge base entry to reflect sensations and not facts.

--- rational_explorer.py
+++ rational_explorer.py
@@ -26,8 +26,8 @@ class RationalExplorer(Explorer):
             elif self.facing == Facing.WEST:
                 target_cell[0] -= 1
 
-            obstacle_clause: Clause = Clause([Sentence("obstacle", "o", literals=target_cell, negated=False)])
-            self.knowledge_base.append(obstacle_clause)
+            bump_clause: Clause = Clause([Sentence("bump", "bu", literals=target_cell, negated=False)])
+            self.knowledge_base.append(bump_clause)
         return bumped
 
     def act(self) -> None:
@@ -92,34 +92,26 @@ class RationalExplorer(Explorer):
 
     def update_knowledge_base(self):
         sensations: List[Sensation] = self.observe()
-        adjacent_cells = self.get_adjacent_cells()
 
-        if sensations[Sensation.BREEZE]:
-            pit_sentences: List[Sentence] = []
-            for cell in adjacent_cells:
-                pit_sentences.append(Sentence("pit", "p", literals=cell, negated=False))
-            self.knowledge_base.append(Clause(pit_sentences))
-        else:
-            for cell in adjacent_cells:
-                self.knowledge_base.append(Clause([Sentence("pit", "p", literals=cell, negated=True)]))
+        # bleh ternary statements
 
-        if sensations[Sensation.STENCH]:
-            wumpus_sentences: List[Sentence] = []
-            for cell in adjacent_cells:
-                wumpus_sentences.append(Sentence("wumpus", "w", literals=cell, negated=False))
-            self.knowledge_base.append(Clause(wumpus_sentences))
-        else:
-            for cell in adjacent_cells:
-                self.knowledge_base.append(Clause([Sentence("wumpus", "w", literals=cell, negated=True)]))
+        stench_sentence: Sentence = Sentence('stench', 's', literals = self.location, negated=False) \
+            if sensations[Sensation.STENCH] \
+            else Sentence('stench', 's', literals = self.location, negated=True)
 
-        if sensations[Sensation.GLIMMER]:
-            gold_sentences: List[Sentence] = []
-            for cell in adjacent_cells:
-                gold_sentences.append(Sentence("gold", "g", literals=cell, negated=False))
-            self.knowledge_base.append(Clause(gold_sentences))
-        else:
-            for cell in adjacent_cells:
-                self.knowledge_base.append(Clause([Sentence("gold", "g", literals=cell, negated=True)]))
+
+        breeze_sentence: Sentence = Sentence('breeze', 'b', literals=self.location, negated=False) \
+            if sensations[Sensation.BREEZE] \
+            else Sentence('breeze', 'b', literals=self.location, negated=True)
+
+
+        glimmer_sentence: Sentence =  Sentence('glimmer', 'gl', literals = self.location, negated=False) \
+            if sensations[Sensation.GLIMMER] \
+            else Sentence('glimmer', 'gl', literals = self.location, negated=True)
+
+        self.knowledge_base.append(Clause([breeze_sentence]))
+        self.knowledge_base.append(Clause([stench_sentence]))
+        self.knowledge_base.append(Clause([glimmer_sentence]))
 
         return
 
@@ -137,52 +129,52 @@ class RationalExplorer(Explorer):
         ~s(x,y) | [w(x+1, y) | w(x-1, y) | w(x, y+1) | w(x, y-1)]
         '''
 
-        # r1_s1: Sentence = Sentence("stench", "s", variables = ["x", "y"], negated = True)
-        # r1_s2: Sentence = Sentence("wumpus", "w", variables = ["x+1", 'y'])
-        # r1_s3: Sentence = Sentence("wumpus", "w", variables=["x-1", 'y'])
-        # r1_s4: Sentence = Sentence("wumpus", "w", variables=["x", 'y+1'])
-        # r1_s5: Sentence = Sentence("wumpus", "w", variables=["x", 'y-1'])
-        # rule1: Clause = Clause([r1_s1, r1_s2, r1_s3, r1_s4, r1_s5])
-        #
-        # '''
-        # Rule 2
-        # (Breeze implies pit in adjacent cell) converted to clause form is
-        # ~b(x,y) | [p(x+1, y) | p(x-1, y) | p(x, y+1) | p(x, y-1)]
-        # '''
-        #
-        # r2_s1: Sentence = Sentence("breeze", "b", variables=["x", "y"], negated=True)
-        # r2_s2: Sentence = Sentence("pit", "p", variables=["x+1", 'y'])
-        # r2_s3: Sentence = Sentence("pit", "p", variables=["x-1", 'y'])
-        # r2_s4: Sentence = Sentence("pit", "p", variables=["x", 'y+1'])
-        # r2_s5: Sentence = Sentence("pit", "p", variables=["x", 'y-1'])
-        # rule2: Clause = Clause([r2_s1, r2_s2, r2_s3, r2_s4, r2_s5])
-        #
-        # """
-        # Rule 3
-        # (glimmer implies gold in adjacent cell) converted to clause form is
-        # ~gl(x,y) | [g(x+1, y) | g(x-1, y) | g(x, y+1) | g(x, y-1)]
-        # """
-        #
-        # r3_s1: Sentence = Sentence("glimmer", "gl", variables=["x", "y"], negated=True)
-        # r3_s2: Sentence = Sentence("gold", "g", variables=["x+1", 'y'])
-        # r3_s3: Sentence = Sentence("gold", "g", variables=["x-1", 'y'])
-        # r3_s4: Sentence = Sentence("gold", "g", variables=["x", 'y+1'])
-        # r3_s5: Sentence = Sentence("gold", "g", variables=["x", 'y-1'])
-        # rule3: Clause = Clause([r3_s1, r3_s2, r3_s3, r3_s4, r3_s5])
-        #
-        # '''
-        # Rule 4
-        # (bump implies obstacle which mean you cant move in that direction anymore) converted to clause form is
-        # ~bu(x,y) | o(x,y)
-        # '''
-        #
-        # r4_s1: Sentence = Sentence("bump", "bu", variables = ["x", "y"], negated = True)
-        # r4_s2: Sentence = Sentence("obstacle", "o", variables = ["x", "y"])
-        # rule4: Clause = Clause(r4_s1, r4_s2)
-        #
-        # self.knowledge_base.set_rules([rule1, rule2, rule3, rule4])
-        self.knowledge_base.set_rules([])
+        r1_s1: Sentence = Sentence("stench", "s", variables = ["x", "y"], negated = True)
+        r1_s2: Sentence = Sentence("wumpus", "w", variables = ["x+1", 'y'])
+        r1_s3: Sentence = Sentence("wumpus", "w", variables=["x-1", 'y'])
+        r1_s4: Sentence = Sentence("wumpus", "w", variables=["x", 'y+1'])
+        r1_s5: Sentence = Sentence("wumpus", "w", variables=["x", 'y-1'])
+        rule1: Clause = Clause([r1_s1, r1_s2, r1_s3, r1_s4, r1_s5])
 
+        '''
+        Rule 2
+        (Breeze implies pit in adjacent cell) converted to clause form is
+        ~b(x,y) | [p(x+1, y) | p(x-1, y) | p(x, y+1) | p(x, y-1)]
+        '''
+
+        r2_s1: Sentence = Sentence("breeze", "b", variables=["x", "y"], negated=True)
+        r2_s2: Sentence = Sentence("pit", "p", variables=["x+1", 'y'])
+        r2_s3: Sentence = Sentence("pit", "p", variables=["x-1", 'y'])
+        r2_s4: Sentence = Sentence("pit", "p", variables=["x", 'y+1'])
+        r2_s5: Sentence = Sentence("pit", "p", variables=["x", 'y-1'])
+        rule2: Clause = Clause([r2_s1, r2_s2, r2_s3, r2_s4, r2_s5])
+
+        """
+        Rule 3
+        (glimmer implies gold in adjacent cell) converted to clause form is
+        ~gl(x,y) | [g(x+1, y) | g(x-1, y) | g(x, y+1) | g(x, y-1)]
+        """
+
+        r3_s1: Sentence = Sentence("glimmer", "gl", variables=["x", "y"], negated=True)
+        r3_s2: Sentence = Sentence("gold", "g", variables=["x+1", 'y'])
+        r3_s3: Sentence = Sentence("gold", "g", variables=["x-1", 'y'])
+        r3_s4: Sentence = Sentence("gold", "g", variables=["x", 'y+1'])
+        r3_s5: Sentence = Sentence("gold", "g", variables=["x", 'y-1'])
+        rule3: Clause = Clause([r3_s1, r3_s2, r3_s3, r3_s4, r3_s5])
+
+        '''
+        Rule 4
+        (bump implies obstacle which mean you cant move in that direction anymore) converted to clause form is
+        ~bu(x,y) | o(x,y)
+        '''
+
+        r4_s1: Sentence = Sentence("bump", "bu", variables = ["x", "y"], negated = True)
+        r4_s2: Sentence = Sentence("obstacle", "o", variables = ["x", "y"])
+        rule4: Clause = Clause([r4_s1, r4_s2])
+
+        self.knowledge_base.set_rules([rule1, rule2, rule3, rule4])
+
+    # TODO: CURRENT IMPLEMENTATION PROBABLY DOESN'T WORK. MAY BE MULTIPLE CLAUSES CONTAINING SAME INFO
     def assign_danger_value(self, coords) -> float:
         wumpus_danger: float = 0
         wumpus_safe_sentence: Sentence = Sentence("wumpus", "w", literals=coords, negated=True)
